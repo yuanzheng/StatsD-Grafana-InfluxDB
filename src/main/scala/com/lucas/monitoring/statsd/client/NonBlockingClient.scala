42025e8dbb63cb3caf8dbe22c092fe8d4e679a90
Lucas Song <yuanzheng@cs.byu.edu>
2019-11-29 23:50:29 +0800

    defined NonBlockingClient, it is used when StatsD has been enabled

--- src/main/scala/com/lucas/monitoring/statsd/client/NonBlockingClient.scala
+++ src/main/scala/com/lucas/monitoring/statsd/client/NonBlockingClient.scala
@@ -1,5 +1,11 @@
 package com.lucas.monitoring.statsd.client
 
-trait NonBlockingClient {
+import java.util.concurrent.Future
+
+/**
+  * A StatsDClient implementation which communicates with the StatsD server on a background thread.  This
+  * implementation is useful when StatsD has been enabled in the application configuration.
+  */
+case class NonBlockingClient() extends StatsDClient[Future[Int]] {
 
 }
