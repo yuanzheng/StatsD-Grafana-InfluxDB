b8a57ee5eb57754d20822c64bd0809ad841ba523
Lucas Song <yuanzheng@cs.byu.edu>
2019-11-29 23:46:49 +0800

    defined statsDConfiguration and ClientConfiguration

--- src/main/scala/com/lucas/monitoring/statsd/client/configuration/ClientConfiguration.scala
+++ src/main/scala/com/lucas/monitoring/statsd/client/configuration/ClientConfiguration.scala
@@ -1,5 +1,22 @@
 package com.lucas.monitoring.statsd.client.configuration
 
-trait ClientConfiguration {
+import com.lucas.monitoring.statsd.configuration.StatsDConfiguration
+
+/**
+  * Singleton for accessing client configurations
+  */
+object ClientConfiguration extends ClientConfiguration
+
+trait ClientConfiguration extends StatsDConfiguration {
+
+    override val predicate = getKey("client")
+
+    /**
+      * Determines if StatsD recording is enabled
+      */
+    lazy val isEnabled = getBoolean("enabled", false)
+
+
+
 
 }
--- src/main/scala/com/lucas/monitoring/statsd/configuration/StatsDConfiguration.scala
+++ src/main/scala/com/lucas/monitoring/statsd/configuration/StatsDConfiguration.scala
@@ -1,5 +1,43 @@
 package com.lucas.monitoring.statsd.configuration
 
-object StatsDConfiguration {
+import com.typesafe.config.ConfigFactory
+
+import scala.util.{Success, Try}
+
+/**
+  * Singleton for accessing all configurations
+  */
+object StatsDConfiguration extends StatsDConfiguration
+
+
+trait StatsDConfiguration {
+
+    /**
+      * Default configuration prefix which will be used if one is not specified
+      */
+    val predicate: String = "statsd.client"
+
+    /**
+      * This can be used if a given configuration does not have a singleton for
+      * accessing the configuration.
+      */
+    lazy val config = ConfigFactory.load()
+
+
+    def getKey(key: String)(implicit predicate: String = this.predicate): String = predicate match {
+        case empty if predicate.isEmpty => key
+        case dotted if predicate.endsWith(".") => predicate + key
+        case _ => predicate + "." + key
+    }
+
+
+    /** Get the configuration boolean value for the given key.
+      *
+      * @param key
+      * @param default
+      * @return
+      */
+    def getBoolean(key: String, default: Boolean = false) = (Try(config.getBoolean(getKey(key))) recoverWith { case _ => Success(default) }).get
+
 
 }
