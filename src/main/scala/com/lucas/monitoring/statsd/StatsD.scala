91ea20f4b7e74d9e52b07cda2e96a346c6953bc7
Lucas Song <yuanzheng@cs.byu.edu>
2019-11-29 23:48:46 +0800

    defined StatsD interface for other program to use git status

--- src/main/scala/com/lucas/monitoring/statsd/StatsD.scala
+++ src/main/scala/com/lucas/monitoring/statsd/StatsD.scala
@@ -1,5 +1,11 @@
 package com.lucas.monitoring.statsd
 
+import client.configuration.ClientConfiguration
+import client.StatsDClient
+
 trait StatsD {
+
+    lazy val configuration: ClientConfiguration = ClientConfiguration
+    lazy val client: StatsDClient[_] = StatsDClient(configuration.isEnabled)
 
 }
