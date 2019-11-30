2bea4e3e0d819167126d42384acc5413726a8daa
Lucas Song <yuanzheng@cs.byu.edu>
2019-11-30 10:38:57 +0800

    StatsD reference is a singleton object

--- src/main/scala/com/lucas/monitoring/statsd/StatsD.scala
+++ src/main/scala/com/lucas/monitoring/statsd/StatsD.scala
@@ -3,6 +3,18 @@ package com.lucas.monitoring.statsd
 import client.configuration.ClientConfiguration
 import client.StatsDClient
 
+/** Provide a reference to StatsD singleton
+  *
+  */
+trait StatsDProvider {
+    lazy val statsd: StatsDClient[_] = StatsD.client
+}
+
+/** StatsD companion object
+  *
+  */
+object StatsD extends StatsD
+
 trait StatsD {
 
     lazy val configuration: ClientConfiguration = ClientConfiguration
