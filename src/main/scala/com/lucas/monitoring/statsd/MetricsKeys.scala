888e5deafe017bebab992bcc786bdf2e44c6c219
Lucas Song <yuanzheng@cs.byu.edu>
2019-11-30 15:34:49 +0800

    updated comments for programmers

--- src/main/scala/com/lucas/monitoring/statsd/MetricsKeys.scala
+++ src/main/scala/com/lucas/monitoring/statsd/MetricsKeys.scala
@@ -1,5 +1,8 @@
 package com.lucas.monitoring.statsd
 
+/** Actually, programmers should defined metric keys for each project along with meaningful values
+  *
+  */
 object MetricsKeys {
 
     val EXECUTED = "executed"
